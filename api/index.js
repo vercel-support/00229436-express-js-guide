e17c600a8df16a0c3873222dcf538c15620aaef3
Nick Vigier <nvigier46@gmail.com>
2024-05-14 13:27:18 +0100

    add 2nd path

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Express on Vercel'));
+app.get('/test', (req, res) => res.send('Test! Express on Vercel'));
 
 app.listen(4001, () => console.log('Server ready on port 3000.'));
 
